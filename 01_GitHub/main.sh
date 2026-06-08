7588068d138e2dc7f6ed141677ec822e0f8800ad
iquasere <maildosequeira@gmail.com>
2026-06-08 14:44:01 +0200

    Proper WORKING_DIR and OUTPUT_DIR paths

--- 01_GitHub/main.sh
+++ 01_GitHub/main.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -euo pipefail
 
-WORKING_DIR=/scratch/jsequeira/jsequeira/github/acceleration-toolbox/01_GitHub
-OUTPUT_DIR=/scratch/jsequeira/jsequeira/pipeline_results
+WORKING_DIR=01_GitHub
+OUTPUT_DIR=pipeline_results
 THREADS=8
 
 bash 01_GitHub/scripts/01-qc.sh "$WORKING_DIR" "$OUTPUT_DIR" "$THREADS"
